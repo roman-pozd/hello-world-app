6bd44519dcbeafde755622e1ee21f7dc1cad7c1f
roman-pozd <roman.pozdnyakov@aiven.io>
2026-03-10 12:48:57 +0200

    fix: update frontend API_URL to backend-v3

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.API_URL || "https://019cd743-6831-7b3a-b12d-df51309a3431-3000.eur-1.stg.rapu.app";
+const API_URL = process.env.API_URL || "https://019cd75c-e5ee-74a7-93c2-cd827cb1c36e-3000.eur-1.stg.rapu.app";
 
 interface LogEntry {
   id: number;
